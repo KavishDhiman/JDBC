c71e75fd61e600b7c3792f3e66916888abf9d744
KavishDhiman <kavishdhiman2003@gmail.com>
2026-02-27 16:46:38 +0530

    Initial project skeleton setup

--- src/main/java/com/bridgelabz/payroll/PayrollApplication.java
+++ src/main/java/com/bridgelabz/payroll/PayrollApplication.java
@@ -1,21 +1,6 @@
 package com.bridgelabz.payroll;
-
-import com.bridgelabz.payroll.service.PayrollDBService;
-
-import java.sql.Connection;
-
 public class PayrollApplication {
-
     public static void main(String[] args) {
-
-        try (Connection connection =
-                     PayrollDBService.getConnection()) {
-
-            System.out.println("Connection Established Successfully!");
-
-        } catch (Exception e) {
-            System.out.println("Connection Failed!");
-            e.printStackTrace();
-        }
+        System.out.println("JDBC Employee Payroll App");
     }
 }
