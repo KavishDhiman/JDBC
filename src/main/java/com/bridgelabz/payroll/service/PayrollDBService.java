b00e7b8e0684ff8de82f1c01a2c397b522f078ad
KavishDhiman <149812635+KavishDhiman@users.noreply.github.com>
2026-02-27 16:52:42 +0530

    Update PayrollDBService.java

--- src/main/java/com/bridgelabz/payroll/service/PayrollDBService.java
+++ src/main/java/com/bridgelabz/payroll/service/PayrollDBService.java
@@ -1,17 +1,1 @@
-package com.bridgelabz.payroll.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class PayrollDBService {
-
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/payroll_service";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Kavish@12";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-}
